0e6ee08358935745aea29c551e11850af45eb544
Akhmed Borshchigov <akhmed9505@gmail.com>
2026-04-21 16:12:47 +0300

    feat(notification): integrate service with cache and message publisher

--- internal/service/notification/service.go
+++ internal/service/notification/service.go
@@ -2,11 +2,12 @@ package notification
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
-	"github.com/google/uuid"
-
 	"github.com/akhmed9505/delayed-notifier/internal/domain"
+	"github.com/akhmed9505/delayed-notifier/internal/infra/redis"
+	"github.com/google/uuid"
 )
 
 type Repository interface {
@@ -15,13 +16,26 @@ type Repository interface {
 	UpdateStatus(ctx context.Context, id uuid.UUID, status domain.NotificationStatus) error
 }
 
-type Service struct {
-	repository Repository
+type Publisher interface {
+	Publish(ctx context.Context, notification domain.Notification) error
 }
 
-func New(repository Repository) *Service {
+type Cache interface {
+	SetStatus(ctx context.Context, id uuid.UUID, status domain.NotificationStatus) error
+	GetStatus(ctx context.Context, id uuid.UUID) (domain.NotificationStatus, error)
+}
+
+type Service struct {
+	repository Repository
+	publisher  Publisher
+	cache      Cache
+}
+
+func New(repository Repository, publisher Publisher, cache Cache) *Service {
 	return &Service{
 		repository: repository,
+		publisher:  publisher,
+		cache:      cache,
 	}
 }
 
@@ -33,16 +47,34 @@ func (s *Service) Create(ctx context.Context, notification domain.Notification) 
 		return uuid.Nil, fmt.Errorf("%s: %w", op, err)
 	}
 
+	notification.ID = id
+
+	_ = s.cache.SetStatus(ctx, id, domain.Pending)
+
+	if err := s.publisher.Publish(ctx, notification); err != nil {
+		return uuid.Nil, fmt.Errorf("%s: publish notification: %w", op, err)
+	}
+
 	return id, nil
 }
 
 func (s *Service) GetStatusByID(ctx context.Context, id uuid.UUID) (domain.NotificationStatus, error) {
 	const op = "notification.service.GetStatusByID"
 
-	status, err := s.repository.GetStatusByID(ctx, id)
+	status, err := s.cache.GetStatus(ctx, id)
+	if err == nil {
+		return status, nil
+	}
+	if !errors.Is(err, redis.ErrCacheMiss) {
+		return "", fmt.Errorf("%s: get status from cache: %w", op, err)
+	}
+
+	status, err = s.repository.GetStatusByID(ctx, id)
 	if err != nil {
 		return "", fmt.Errorf("%s: %w", op, err)
 	}
+
+	_ = s.cache.SetStatus(ctx, id, status)
 
 	return status, nil
 }
@@ -53,6 +85,8 @@ func (s *Service) UpdateStatus(ctx context.Context, id uuid.UUID, status domain.
 	if err := s.repository.UpdateStatus(ctx, id, status); err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
+
+	_ = s.cache.SetStatus(ctx, id, status)
 
 	return nil
 }
