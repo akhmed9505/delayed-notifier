2be44d26513c101166578a72351a72a12a234e37
Akhmed Borshchigov <akhmed9505@gmail.com>
2026-04-22 14:09:08 +0300

    feat(http): add http server

---
+++ internal/delivery/http/server.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/wb-go/wbf/ginext"
+)
+
+func NewServer(addr string, router *ginext.Engine) *http.Server {
+	return &http.Server{
+		Addr:    addr,
+		Handler: router,
+	}
+}
